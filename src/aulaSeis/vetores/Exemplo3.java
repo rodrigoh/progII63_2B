911ce390e1658d9583003b7dc5ca1baa1d8b7d57
Rodrigo Henrich <rodrigohenrich@faccat.br>
2024-04-12 11:38:03 -0300

    Exemplo vetores.

---
+++ src/aulaSeis/vetores/Exemplo3.java
@@ -0,0 +1,12 @@
+package aulaSeis.vetores;
+
+public class Exemplo3 {
+	public static void main(String[] args) {
+		//Vetor com 7 elementos e indices de 0 a 6
+		int[] vetor = {1,2,3,4,6,10};
+		System.out.println("Dados no vetor...");
+		for(int i : vetor){
+			System.out.print(i+" ");
+		}
+	}
+}
