e63782c0f3913f9c3dd24ae92064befe46b84225
rodrigoh <rodrigohenrich@faccat.br>
2024-07-19 09:06:35 -0300

    Exemplos aula 21

---
+++ src/aula21/exemploHeranca/Engenheiro.java
@@ -0,0 +1,19 @@
+package aula21.exemploHeranca;
+
+public class Engenheiro extends Funcionario{
+	private String crea;
+
+	public Engenheiro(String nome, String sobrenome, int matricula, String crea){
+		super(nome,sobrenome,matricula);
+		this.crea = crea;
+
+	}
+
+	public String getCrea() {
+		return crea;
+	}
+
+	public void setCrea(String crea) {
+		this.crea = crea;
+	}
+}
