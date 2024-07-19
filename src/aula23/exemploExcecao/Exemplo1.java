defcc2276b6b383c6ed97dc0bbb32a96bee6ab9d
rodrigoh <rodrigohenrich@faccat.br>
2024-07-19 09:07:16 -0300

    Exemplos aula 23

---
+++ src/aula23/exemploExcecao/Exemplo1.java
@@ -0,0 +1,43 @@
+package aula23.exemploExcecao;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exemplo1 {
+
+	public static int leNumero(String mensagem){
+		Scanner ler = new Scanner(System.in);
+		int num=0;
+		boolean correto;
+		do{
+			try{
+				System.out.print(mensagem);
+				num = ler.nextInt();
+				correto = true;
+			}
+			catch (InputMismatchException e){
+				correto = false;
+				ler.next();
+				System.out.println("Valor inválido!");
+			}
+		}
+		while(!correto);
+		return num;
+	}
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n1 = leNumero("Digite um valor para n1: ");
+		int n2 = leNumero("Digite um valor para n2: ");
+		try {
+			int resposta = n1 / n2;
+			System.out.println(resposta);
+		}
+		catch (InputMismatchException erro){
+			System.out.println("Os valores precisam ser números");
+		}
+		catch (ArithmeticException erro){
+			System.out.println("Não é possível dividir por zero");
+		}
+	}
+
+}
---
+++ src/aula23/exemploExcecao/Exemplo2.java
@@ -0,0 +1,10 @@
+package aula23.exemploExcecao;
+
+public class Exemplo2 {
+
+	public int divide(int n1, int n2) throws ArithmeticException{
+		if(n2==0)
+			throw new ArithmeticException("Não é possível dividir por zero");
+		return n1/n2;
+	}
+}
