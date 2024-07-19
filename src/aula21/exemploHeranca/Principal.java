16ab30d31ccaa5aff74e71f1e8c7549fd9dea003
rodrigoh <rodrigohenrich@faccat.br>
2024-07-19 09:06:16 -0300

    Exemplos aula 21

---
+++ src/aula21/exemploHeranca/Principal.java
@@ -0,0 +1,18 @@
+package aula21.exemploHeranca;
+
+public class Principal {
+	public static void main(String[] args) {
+		Motorista m1 = new Motorista("Jonas","Silva",1258);
+
+//		m1.setNome("Jonas");
+//		m1.setSobrenome("Silva");
+//		m1.setMatricula(1258);
+
+		Engenheiro e1 = new Engenheiro("Ryan", "koch",1289,"488816113");
+//		e1.setNome("Ryan");
+//		e1.setSobrenome("koch");
+//		e1.setMatricula(1289);
+//		e1.setCrea("488816113");
+
+	}
+}
